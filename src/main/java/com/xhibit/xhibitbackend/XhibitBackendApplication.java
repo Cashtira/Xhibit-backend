b1d69baeaaa5fcfca17e0c11b949d1e4161f2d06
Cashtira <22520599@gm.uit.edu.vn>
2025-04-22 23:21:54 +0700

    Initial Commit

---
+++ src/main/java/com/xhibit/xhibitbackend/XhibitBackendApplication.java
@@ -0,0 +1,13 @@
+package com.xhibit.xhibitbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XhibitBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XhibitBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xhibit/xhibitbackend/XhibitBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xhibit.xhibitbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XhibitBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
